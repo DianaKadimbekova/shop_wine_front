23e6e8bad4a55f67686e76e2ef3ea4d5dd710086
Diana Kadimbekova <lebedenko202@gmail.com>
2025-05-18 14:36:32 +0300

    add architecture

---
+++ src/components/Header/ShopWineHeader.tsx
@@ -0,0 +1,19 @@
+export const ShopWineHeader = () => {
+  return (
+    <>
+      <header>
+        <div className="header__top">
+          <a href="#">
+            <img src="" alt="Logo" />
+          </a>
+          <a href="#">Wine catalog</a>
+          <a href="#">Delivery and contacts</a>
+          <a href="#">Wine Accessories</a>
+          <input type="text" />
+          <button>en</button>
+        </div>
+        <div className="header__bottom"></div>
+      </header>
+    </>
+  );
+};
