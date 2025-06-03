9f61f0965b885752fb08c833367bcc22d1617e6a
Diana Kadimbekova <lebedenko202@gmail.com>
2025-06-03 12:48:26 +0300

    add api wine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { ShopWineFooter } from "./components/Footer/ShopWineFooter";
 import { ShopWineHeader } from "./components/Header/ShopWineHeader";
+import { ShopWineList } from "./components/List/ShopWineList.tsx";
 
 function App() {
   return (
     <>
       <ShopWineHeader />
+      <ShopWineList />
+      <ShopWineFooter />
     </>
   );
 }
--- src/components/List/ShopWineList.tsx
+++ src/components/List/ShopWineList.tsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import { ShopWineModal } from "../Modal/ShopWineModal";
+import type { ShopWineModalType } from "../Modal/ShopWineModalType";
+
 export const ShopWineList = () => {
-  return <></>;
+  const [wines, setWines] = useState<ShopWineModalType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const fetchWines = async () => {
+      try {
+        const response = await fetch(
+          "https://back-vonoselecto-bedagphgf7cgeqf3.uksouth-01.azurewebsites.net/api/shop/wine/"
+        );
+        if (!response.ok) throw new Error("Failed to fetch wines");
+        const data = await response.json();
+        setWines(data.results);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchWines();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!wines.length) return <div>No wines found</div>;
+
+  return (
+    <>
+      <button></button>
+      <div>
+        {wines.map((wine) => (
+          <ShopWineModal key={wine.id} id={wine.id} />
+        ))}
+      </div>
+      <button></button>
+    </>
+  );
 };
